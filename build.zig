108b37c91c797f90038a184de32408a2bf094e8e
pedropark99 <pedropark99@gmail.com>
2024-05-25 22:29:37 -0300

    Add content

--- build.zig
+++ build.zig
@@ -15,6 +15,8 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    const libraw = b.addStaticLibrary(.{ .name = "libraw", .target = target, .optimize = optimize });
+
     const lib = b.addStaticLibrary(.{
         .name = "libraw-zig",
         // In this case the main source file is merely a path, however, in more
--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,12 @@
 const std = @import("std");
+const raw = @cImport(@cInclude("libraw/libraw.h"));
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+    const libraw = raw.lib_raw_init();
+    _ = libraw;
+    const img_path = "/home/pedro-dev/Pictures/Fotos-Edicao/IMG_0005.CR3";
+    _ = img_path;
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
 }
 
 test "simple test" {
